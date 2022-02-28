66f2823dca4d1f0b97244df07ffd2670271b5fda
Bálint Molnár <molnar.balint23@gmail.com>
2022-02-28 02:57:49 +0000

    Fixing the display of the map
    - Markers are to be added for posts
    - Code will be cleaned
    - Posts should include GPS coordinates to be displayed on the map

--- src/scenes/map.js
+++ src/scenes/map.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { map } from 'leaflet';
 
+import {Dimensions, Image, View, Text, FlatList, Button, Alert} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
+
+import {AppStyle} from '../styles';
+import Colours from '../styles'
+
+import './App.css'
+
+import SearchBar from './search';
 
 
 function MapScreen({ navigation }) {
+  
+
+
+ 
+
 
 
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
 
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+       <Button
+          icon={<Ionicons name='home' color='red' size='15'/>}
+          title='home'
+          onPress={() => document.getElementById('reset').click()}
+        />
+        <div>
+        <style scoped>@import "https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"  </style>
       <MapContainer center={[51.505, -0.09]} zoom={13} >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -25,8 +45,8 @@ function MapScreen({ navigation }) {
           </Popup>
         </Marker>
       </MapContainer>
-
-      <Text>Map map!</Text>
+      </div>
+      <Text>Markers for posts are to be added</Text>
     </View>
   );
 }
