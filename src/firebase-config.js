01eed0faa66854579f9208442ab381f616e9d539
mm2504 <mm2504@cam.ac.uk>
2022-02-28 23:15:23 +0000

    Adding the posts to the database and toggling upvotes

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import { getFirestore, doc, getDoc, setDoc, addDoc, collection } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDyFygporWON-sAA5rJ17xbiTXi-vJhtm8",
@@ -19,28 +19,93 @@ export const FirebaseAuth = getAuth(FirebaseApp);
 export const FirebaseDB = getDatabase(FirebaseApp);
 export const db = getFirestore();
 
-export async function getUserDoc(user) {            //If a user object is provided it will return the corresponding data snap.
+export async function getUserDoc(user) {            //If a user object is provided it will return the corresponding data.
     if (user) {
         const docRef = doc(db, 'Users', user.uid);
         const docSnap = await getDoc(docRef);
 
         if (docSnap.exists()) {
             console.log("Document data:", docSnap.data());
-            return docSnap;
+            return docSnap.data();
         } else {
             setDoc(doc(db, "Users", user.uid), {
             });
             docSnap = await getDoc(docRef);
             console.log(docSnap);
-            return docSnap;
+            return docSnap.data();
         }
     } else {
         return undefined;
     }
 }
 
-export async function setUserDoc(user, params) {
+export async function getPostDoc(postId) {            //If a post id is provided it will return the corresponding data.
+    if (user) {
+        const docRef = doc(db, 'Posts', postId);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            //console.log("Document data:", docSnap.data());
+            return docSnap.data();
+        } else {
+            return undefined;
+        }
+    } else {
+        return undefined;
+    }
+}
+
+export async function setUserDoc(params, user) {
     if (user) {
         setDoc(doc(db, "Users", user.uid), params, { merge: true });
     }
+}
+
+export async function setPostDoc(params, postId) {
+    console.log(postId);
+    setDoc(doc(db, "Posts", postId), params, { merge: true });
+}
+
+export async function uploadPostToDB(postJSON, user) {
+    if (user) {
+        const userDoc = await getUserDoc(user);
+
+        postJSON.userUID = user.uid;
+        postJSON.Upvotes = 0;
+        postJSON.Upvoters = 0;
+        let userPostData = userDoc.Posts;
+        let postRef = await addDoc(collection(db, 'Posts'), postJSON);
+        if (!userPostData) userPostData = []
+        userPostData.push(postRef.id);
+        console.log("Upload logs");
+        console.log(userPostData);
+        setUserDoc({ 'Posts': userPostData }, user);
+    }
+}
+
+export async function getUser() {
+    return FirebaseAuth.currentUser;
+}
+
+export async function togglePostUpvote(postId, user) {
+    const userDoc = await getUserDoc(user);
+    const postDoc = await getPostDoc(postId);
+    let upvotedPosts = userDoc.upvotedPosts;
+    let upvoters = postDoc.Upvoters;
+    let upvotes = postDoc.Upvotes;
+    if (!upvotedPosts) upvotedPosts = [];
+    if (!upvoters) upvoters = [];
+    if (!upvotes) upvotes = 0;
+    let postUpvoted = upvotedPosts.includes(postId);
+    if (!postUpvoted) {
+        upvotedPosts.push(postId);
+        upvoters.push(user.uid);
+        upvotes += 1;
+    } else {
+        upvotedPosts = upvotedPosts.filter(e => e !== postId);
+        upvoters = upvoters.filter(e => e !== user.uid);
+        upvotes -= 1;
+    }
+    setUserDoc({ 'upvotedPosts': upvotedPosts }, user);
+    setPostDoc({ 'Upvoters': upvoters, 'Upvotes': upvotes }, postId);
 }
--- src/scenes/home.js
+++ src/scenes/home.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Dimensions, Image, View, Text, FlatList, Button, Alert} from 'react-native';
+import { Dimensions, Image, View, Text, FlatList, Button, Alert } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-import {AppStyle} from '../styles';
+import { AppStyle } from '../styles';
 import Colours from '../styles'
 import { TouchableHighlight } from 'react-native-gesture-handler';
+import { getUser, togglePostUpvote } from '../firebase-config';
 
 
-class Post extends React.Component{
+class Post extends React.Component {
 
   constructor(props) {
     super(props);
@@ -23,43 +24,45 @@ class Post extends React.Component{
     if (this.props.mediaType == 'image') {
       Image.getSize(this.props.mediaSource, (w, h) => {
         const displayWidth = Dimensions.get('window').width
-        this.setState({mediaWidth: displayWidth, mediaHeight: ((h * displayWidth) / w)})
+        this.setState({ mediaWidth: displayWidth, mediaHeight: ((h * displayWidth) / w) })
       })
     }
   }
 
   onUpvotePressed = () => {
-    {this.state.postUpvoted ? 
-      this.setState({postUpvoted: false, upvotes: this.state.upvotes-1})
-      : this.setState({postUpvoted: true, upvotes: this.state.upvotes+1})}
+    {
+      this.state.postUpvoted ?
+        this.setState({ postUpvoted: false, upvotes: this.state.upvotes - 1 })
+        : this.setState({ postUpvoted: true, upvotes: this.state.upvotes + 1 })
+    }
   }
 
   render() {
     return (
       <View style={AppStyle.post}>
         <Text style={AppStyle.postHeader}>
-          {this.props.author}, 
+          {this.props.author},
           <Text style={AppStyle.primary}> {this.props.location}</Text>
         </Text>
-        {this.props.mediaType == 'image' ? 
-         <Image
-          style={{width: this.state.mediawidth, height: this.state.mediaHeight}} 
-          source={{uri: this.props.mediaSource}} 
-          /> 
-        : null}
+        {this.props.mediaType == 'image' ?
+          <Image
+            style={{ width: this.state.mediawidth, height: this.state.mediaHeight }}
+            source={{ uri: this.props.mediaSource }}
+          />
+          : null}
         <Text style={AppStyle.postTags}>
           Tags: {this.props.tags.join(', ')}
         </Text>
         <Text style={AppStyle.postTail}>
           {this.props.description}
         </Text>
-        <Text style={AppStyle.postUpvoteBar}> 
-          <TouchableHighlight onPress = {this.onUpvotePressed} underlayColor='white'>
+        <Text style={AppStyle.postUpvoteBar}>
+          <TouchableHighlight onPress={this.onUpvotePressed} underlayColor='white'>
             {this.state.postUpvoted ?
-              <Ionicons name="heart" size={16} color={Colours.PRIMARY}/>
-            : <Ionicons name="heart-outline" size={16} color={Colours.PRIMARY}/>}
+              <Ionicons name="heart" size={16} color={Colours.PRIMARY} />
+              : <Ionicons name="heart-outline" size={16} color={Colours.PRIMARY} />}
           </TouchableHighlight>
-        {''} {this.state.upvotes} upvotes
+          {''} {this.state.upvotes} upvotes
         </Text>
       </View>
     )
@@ -91,42 +94,47 @@ const posts = [
   },
 ]
 
-const renderPost = ({item}) => {
+const renderPost = ({ item }) => {
   return (
-    <Post 
-    author={item.author} 
-    description={item.description} 
-    location={item.location}
-    tags={item.tags}
-    mediaType={item.mediaType}
-    mediaSource={item.mediaSource} 
-    upvotes={item.upvotes}
-    postUpvoted={item.postUpvoted}
+    <Post
+      author={item.author}
+      description={item.description}
+      location={item.location}
+      tags={item.tags}
+      mediaType={item.mediaType}
+      mediaSource={item.mediaSource}
+      upvotes={item.upvotes}
+      postUpvoted={item.postUpvoted}
     />
   )
+}
+
+async function Test() {
+  user = await getUser();
+  togglePostUpvote('zHOYCAQRtvvGDgchSxFp', user);
 }
 
 // TODO: Move styling to styles.js
 // TODO: Add navigator for home button and top navigation bar.
 // TODO: Replace home button with TouchableHighlight and Icon
-function HomeScreen({navigation}) {
+function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex:1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor:'#C0C0C0' }}>
-      <View style={{justifyContent: 'center', alignItems: 'center', height:80, width: '100%', backgroundColor:Colours.PRIMARY}}>
+    <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: '#C0C0C0' }}>
+      <View style={{ justifyContent: 'center', alignItems: 'center', height: 80, width: '100%', backgroundColor: Colours.PRIMARY }}>
         <Button
-          icon={<Ionicons name='home' color='red' size='15'/>}
+          icon={<Ionicons name='home' color='red' size='15' />}
           title='home'
           onPress={() => Alert.alert('Home button pressed')}
         />
       </View>
-      <View style={{flex:1, width: '100%'}}>
+      <View style={{ flex: 1, width: '100%' }}>
         <FlatList
           data={posts}
           renderItem={renderPost}
           keyExtractor={(item) => item.key}
         />
       </View>
-      
+
     </View>
   );
 }
