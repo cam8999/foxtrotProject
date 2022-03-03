51615c8ad27321b9a1d6da1e56398c9ee0b3052f
florencelm <florialuna@icloud.com>
2022-03-03 11:52:07 +0000

    Formats profile page
    - Adds basic formatting
    - Doesn't yet display posts, isn't fully functional

--- src/styles/index.js
+++ src/styles/index.js
@@ -32,6 +32,48 @@ export const AppStyle = StyleSheet.create({
     padding: 10,
     paddingTop: 5,
   },
+  profile: {
+    overflow:'hidden',
+    borderRadius: 15,
+    marginTop: 10,
+    width: '100%',
+  },
+  profileHeader: {
+    padding: 10,
+    backgroundColor: 'white',
+    width: '100%',
+    flexDirection: 'row'
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black'
+  },
+  profileComponent: {
+    marginTop: 3,
+    padding: 10,
+    backgroundColor: 'white',
+    color: '#C9C9C9',
+  },
+  profileDescription: {
+    marginTop: 3,
+    padding: 10,
+    backgroundColor: '#E6E6E6',
+    color: '#545454',
+    fontStyle: 'italic',
+  },
+  button: {
+    backgroundColor: Colours.PRIMARY,
+    paddingVertical: 7,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    elevation: 0,
+    margin: 10,
+  },
+  buttonTitle: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
 });
 
 export default Colours;
